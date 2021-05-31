a2885e7da254c799de58ef1ee56d7980bb4b54d1
Henson Kudi Amah <amahkudi@gmail.com>
2021-05-31 09:42:03 +0400

    updated port

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 const PORT = process.env.PORT || 'http://localhost:5000'
 
 export const baseURL = axios.create({
-    baseURL: PORT
+    baseURL: 'https://hk-solutions-accounting-app.herokuapp.com/'
 })
