458c969612062abdb6b268e36fd87c01e9f459e0
Henson Kudi Amah <amahkudi@gmail.com>
2022-02-14 00:40:22 +0400

    updating user details infos

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -4,7 +4,7 @@ const server = process.env.REACT_APP_SERVER
 const localhost = process.env.REACT_APP_LOCAL_HOST
 
 export const baseURL = axios.create({
-    baseURL: localhost,
+    baseURL: server,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
