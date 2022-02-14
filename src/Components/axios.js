682a01805b793c0badf86a66e343ad029093ebd9
Henson Kudi Amah <amahkudi@gmail.com>
2022-02-14 20:24:16 +0400

    changes server port

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const server = process.env.REACT_APP_SERVER
-const localhost = process.env.REACT_APP_LOCAL_HOST
+
 
 export const baseURL = axios.create({
-    baseURL: localhost,
+    baseURL: server,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
